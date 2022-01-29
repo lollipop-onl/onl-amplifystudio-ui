fb445ef2b22908ca59d208aed7b53943bea93032
lollipop-onl <lollipop@simochee.net>
2022-01-29 12:39:13 +0900

    🔧 setup Prettier

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,14 @@
 /** @type {import('@storybook/core-common').StorybookConfig} */
 const config = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/web-components",
-  "core": {
-    "builder": "storybook-builder-vite"
-  }
-}
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/web-components',
+  core: {
+    builder: 'storybook-builder-vite',
+  },
+};
 
 module.exports = config;
