f9338f5b527a4f57c539bc74c4d51736783d7552
lollipop-onl <lollipop@simochee.net>
2022-01-29 18:30:17 +0900

    ⚡️ add variables

--- src/components/onl-header.ts
+++ src/components/onl-header.ts
@@ -1,14 +1,14 @@
 import { LitElement, html, css, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import common from '~/styles/common.css?inline';
+import superStyles from '~/styles/super.css?inline';
 import styles from './onl-header.css?inline';
 
 @customElement('onl-header')
 export class OnlHeader extends LitElement {
-  static styles = [unsafeCSS(common), unsafeCSS(styles)];
+  static styles = [unsafeCSS(superStyles), unsafeCSS(styles)];
 
   /** ユーザー名 */
-  @property({ type: String }) name!: string;
+  @property({ type: String }) name: string | null = null;
 
   render() {
     return html`<div><p>Hello World ${this.name}</p></div>`
