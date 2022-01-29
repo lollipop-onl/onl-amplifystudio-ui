882fc7253be2da42e5c57ad13e9710e0c2e9f04f
lollipop-onl <lollipop@simochee.net>
2022-01-29 14:27:02 +0900

    🔧 setup Postcss 8 with Webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,52 @@
+const path = require('path');
+
+const castArray = (input) => Array.isArray(input) ? input : [input];
+
 /** @type {import('@storybook/core-common').StorybookConfig} */
 const config = {
   stories: [
     '../src/**/*.stories.ts',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    // {
+    //   name: '@storybook/addon-postcss',
+    //   options: {
+    //     postcssLoaderOptions: {
+    //       postcssOptions: {
+    //         config: path.join(__dirname, '..', 'postcss.config.js'),
+    //       },
+    //       implementation: require('postcss'),
+    //     }
+    //   },
+    // }
+  ],
   framework: '@storybook/web-components',
   core: {
-    builder: 'storybook-builder-vite',
+    builder: 'webpack5',
+  },
+  webpackFinal(config) {
+    config.module.rules.forEach((rule) => {
+      console.log(rule)
+
+      if (!castArray(rule.test).some((test) => test.test('.css'))) return;
+
+      rule.use = [
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+            exportType: 'string',
+          },
+        },
+        {
+          loader: 'postcss-loader',
+        }
+      ]
+    })
+
+    return config;
   },
 };
 
--- src/components/onl-header.ts
+++ src/components/onl-header.ts
@@ -1,12 +1,32 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import styles from './onl-header.css';
+
+console.log(styles)
 
 @customElement('onl-header')
 export class OnlHeader extends LitElement {
+  static styles = [
+    unsafeCSS(styles),
+    css`
+      p {
+        color: red;
+        -webkit-line-clamp: 3;
+        line-clamp: 2;
+      }
+
+      div {
+        & > p {
+          font-weight: bold;
+        }
+      }
+    `
+  ]
+
   @property({ type: String }) name!: string;
 
   render() {
-    return html`<p>Hello World ${this.name}</p>`
+    return html`<div><p>Hello World ${this.name}</p></div>`
   }
 }
 
